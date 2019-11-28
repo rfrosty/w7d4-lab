7aee3e89c38e9cdbe65f1b318e9fdd62678bb53c
Ryan <ryanfrost2015@gmail.com>
2019-11-28 11:49:41 +0000

    Fetches generationmix

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template lang="html">
 <div class="">
   <p>Hello World</p>
+  <span>{{ data }}</span>
+  <!-- <energy-list></energy-list> -->
 </div>
 </template>
 
 <script>
+
+// import EnergyList from './components/energy'
+
 export default {
   name: 'app',
 
+  data() {
+    return {
+      data: [],
+      selectedData: null
+    }
+  },
 
 
+  mounted () {
+    fetch('https://api.carbonintensity.org.uk/generation')
+    .then(response => response.json())
+    .then(one => one.data)
+    .then(two => two.generationmix)
+    // .then(three => three[0])
+    .then(data => this.data = data)
 
-
-  // mounted () {
-  //   fetch('')
-  //   .then(response => response.json())
-  //   .then(data => )
-  // },
+  },
 
 }
 </script>
